f75d4e7e349b45359c77a20c0b04179953446749
Bryant Chow <bryant.h.c@gmail.com>
2020-12-29 23:38:00 +1300

    pvpyplot finished

--- paraview/pvpyplot.py
+++ paraview/pvpyplot.py
@@ -8,8 +8,24 @@ import sys
 from glob import glob
 from paraview.simple import *
 
+
+# 'Global' constants set here
+COLOR_TABLE = {"k": [0., 0., 0.], "w": [1., 1., 1.], "r": [1., 0., 0.],
+               "b": [0., 0., 1.], "g": [0., 1., 0.], "y": [1., 1., 0.],
+               "o": [1., .5, 0.], "c": [0., 1., 1.], "gray": [.5, .5, .5]}
+
+FONTSIZE = 15
+COLOR = COLOR_TABLE["k"]
 """
 Preset parameters that should stay constant among certain file types
+
+:cbar_title (str): Label for the colorbar
+:colormap (str): Colormap to define LUT
+:invert_cmap (bool): Invert the original bounds of the colormap
+:center_cmap (bool): Ensure that the middle value of the colormap is 0
+:range_label_format (str): String formatter for the range bounds on colorbar
+:round_base (int): Round range labels to a base value, if None, no rounding
+:constant_bounds (bool): Keep the colorbar bounds constant for each screenshot
 """
 PRESETS = {
         "model_vp": 
@@ -19,6 +35,7 @@ PRESETS = {
              "center_cmap": False,
              "range_label_format": "%.1f",
              "round_base": 10,
+             "constant_bounds": False,
              },
         "model_vs": 
             {"cbar_title": "Vs [m/s]",
@@ -27,38 +44,43 @@ PRESETS = {
              "center_cmap": False,
              "range_label_format": "%.1f",
              "round_base": 10,
+             "constant_bounds": False,
              },
         "gradient_vp_kernel": 
             {"cbar_title": "Vp Gradient [m^-2 s^2]",
              "colormap": "Cool to Warm (Extended)",
-             "invert_cmap": False,
+             "invert_cmap": True,
              "center_cmap": True,
              "range_label_format": "%.1E",
              "round_base": None,
+             "constant_bounds": True,
              },
         "gradient_vs_kernel": 
             {"cbar_title": "Vs Gradient [m^-2 s^2]",
              "colormap": "Cool to Warm (Extended)",
-             "invert_cmap": False,
+             "invert_cmap": True,
              "center_cmap": True,
              "range_label_format": "%.1E",
              "round_base": None,
+             "constant_bounds": True,
              },
         "update_vp": 
-            {"cbar_title": "Vp Net Update [%]",
+            {"cbar_title": "Vp Net Update [ln(m/m00)]",
              "colormap": "Blue Orange (divergent)",
-             "invert_cmap": False,
+             "invert_cmap": True,
              "center_cmap": True,
-             "range_label_format": "%.03f",
+             "range_label_format": "%.02f",
              "round_base": None,
+             "constant_bounds": True,
              },
         "update_vs": 
-            {"cbar_title": "Vs Net Update [%]",
+            {"cbar_title": "Vs Net Update [ln(m/m00)]",
              "colormap": "Blue Orange (divergent)",
-             "invert_cmap": False,
+             "invert_cmap": True,
              "center_cmap": True,
-             "range_label_format": "%.03f",
+             "range_label_format": "%.02f",
              "round_base": None,
+             "constant_bounds": True,
              },
         "ratio_poissons": 
             {"cbar_title": "Poisson's Ratio",
@@ -67,14 +89,16 @@ PRESETS = {
              "center_cmap": False,
              "range_label_format": "%.1f",
              "round_base": None,
+             "constant_bounds": True,
              },
         "ratio_vpvs": 
             {"cbar_title": "Vp/Vs Ratio",
-             "colormap": "",
+             "colormap": "Cool to Warm (Extended)",
              "invert_cmap": False,
              "center_cmap": False,
-             "range_label_format": "%.01f",
+             "range_label_format": "%.1f",
              "round_base": None,
+             "constant_bounds": True,
              },
         }
 
@@ -101,49 +125,56 @@ def slice_plane(vtk, depth):
     # Apply the slice and render the new view
     renderView = GetActiveViewOrCreate("RenderView")
     sliceDisplay = Show(slice_vtk, renderView)
-    RenameSource(f"{depth}km", slice_vtk)
+    RenameSource(f"z_{depth}km", slice_vtk)
 
     # Hide the plane widget
     Hide3DWidgets(proxy=slice_vtk)
 
     return slice_vtk
 
+def make_ruler(point1, point2, label=""):
+    """
+    Generate a ruler to be used as a scalebar
+    """
+    renderView = GetActiveViewOrCreate("RenderView")
+
+    ruler = Ruler()
+    ruler.Point1 = point1
+    ruler.Point2 = point2
+    rulerDisplay = Show(ruler, renderView, "RulerSourceRepresentation")
+    rulerDisplay.LabelFormat = label
+    rulerDisplay.AxisColor = COLOR
+    rulerDisplay.Color = COLOR
+    rulerDisplay.FontSize = FONTSIZE
+
+    Hide3DWidgets(proxy=ruler)
+
+    return ruler
+
+
 # ============================== USER PARAMETERS ===============================
+
 # File ID's specifying the chosen files
 state_fid = "/Users/Chow/Documents/academic/vuw/forest/forest.pvsm"
+
+# Common path for finding and saving files
+# base_path = "/Users/Chow/Documents/academic/vuw/forest/birch/"
+base_path = os.path.abspath("../..")  # assuming directory structure here
 
 # Allow for user-input files on the command line
 if len(sys.argv) > 1:
     data_fids = sys.argv[1:]
 else:
-    data_fids = glob("/Users/Chow/Documents/academic/vuw/forest/birch/vtk/"
-                     "model/model*0007*vs*")
+    data_fids = glob(os.path.join(base_path, "vtk", "model", "ratio*0007*vs*"))
 
 # Define where the slice the model in the XZ plane, surface implicit
-slices = ["surface", 2, 4, 6, 8, 10, 15, 20, 25, 30, 50]
+slices = ["surface", 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 25, 30, 50]
+slices = ["surface"]
 
 # Save path for screenshots
-path_out = "/Users/Chow/Documents/academic/vuw/forest/birch/figures"
+path_out = os.path.join(base_path, "figures")
 view_size = [1037, 813]
 # ==============================================================================
-
-# Constantly used to render views
-renderView = GetActiveViewOrCreate("RenderView")
-renderView.ViewSize = view_size
-
-# ACTIVE STATE: Load an active state that has common traits, e.g. src-rcv glyphs
-servermanager.LoadState(state_fid)
-for source in ["receivers", "src_epicenter", "coast"]:
-    vtk_ = FindSource(source)
-    vtkDisplay_ = Show(vtk_, renderView, "GeometryRepresentation")
-    vtkDisplay_.SetRepresentationType("Surface With Edges")
-    vtkDisplay_.LineWidth = 2.0
-    vtkDisplay_.Opacity = 0.4
-    if source == "src_epicenter":
-        vtkDisplay_.AmbientColor = [0., 1., 0.]  # Green
-        vtkDisplay_.DiffuseColor = [0., 1., 0.]
-    elif source == "coast":
-        vtkDisplay_.PointSize = 1.5
 
 for data_fid in data_fids:
     # Define the output directory to save figures
@@ -158,18 +189,56 @@ for data_fid in data_fids:
     assert(preset in PRESETS), f"{preset} does not match preset keys"
     preset = PRESETS[preset]
 
+    # Constantly used to render views
+    renderView = GetActiveViewOrCreate("RenderView")
+    renderView.ViewSize = view_size
+
+    # ACTIVE STATE: Load an active state with glyphs, coastline etc.
+    servermanager.LoadState(state_fid)
+    for source in ["receivers", "src_epicenter", "coast"]:
+        vtk_ = FindSource(source)
+        vtkDisplay_ = Show(vtk_, renderView, "GeometryRepresentation")
+        vtkDisplay_.SetRepresentationType("Surface With Edges")
+        vtkDisplay_.LineWidth = 2.0
+        vtkDisplay_.Opacity = 0.4
+        if source == "src_epicenter":
+            vtkDisplay_.AmbientColor = COLOR_TABLE["g"]
+            vtkDisplay_.DiffuseColor = COLOR_TABLE["g"]
+        elif source == "coast":
+            vtkDisplay_.PointSize = 1.5
+
     # Plot the entire model volume
     vtk = OpenDataFile(data_fid)
+    vol_vmin, vol_vmax = vtk.PointData.GetArray(0).GetRange(0)
     RenameSource("surface", vtk)
     Show(vtk)
     ResetCamera()
 
-    # ANNOTATION: Create annotation to be filled in at each depth slice
+    # ANNOTATIONS: Depth slice annotation
+    # Depth slice annotation
     text = Text()
     textDisplay = Show(text, renderView)
-    textDisplay.Position = [0.65, 0.1]
-    textDisplay.FontSize = 40
+    textDisplay.Position = [0.625, 0.1]
+    textDisplay.FontSize = FONTSIZE * 2
+    textDisplay.Color = COLOR
+
+    # File name annotation
+    text_tag = Text()
+    text_tag.Text = save_fid
+    textTagDisplay = Show(text_tag, renderView)
+    textTagDisplay.Position = [0.65, 0.95]
+    textTagDisplay.FontSize = FONTSIZE
+    textTagDisplay.Color = COLOR
+
     Render()
+
+    # RULER: Create rulers as scalebar, points determined manually
+    ruler_origin = [188068, 5722936, 0] 
+    ruler_x = [288068, 5722936, 0]
+    ruler_y = [188068, 5622936, 0]
+
+    make_ruler(ruler_origin, ruler_x, "100km")
+    make_ruler(ruler_origin, ruler_y)
 
     # CAMEAR: Move camera a bit closer
     camera = GetActiveCamera()
@@ -195,8 +264,10 @@ for data_fid in data_fids:
     cbar.AddRangeLabels = 1
     cbar.ScalarBarThickness = 35
     cbar.ScalarBarLength = 0.15
-    cbar.TitleFontSize = 15
-    cbar.LabelFontSize = 15
+    cbar.TitleFontSize = FONTSIZE
+    cbar.LabelFontSize = FONTSIZE
+    cbar.TitleColor = COLOR
+    cbar.LabelColor = COLOR
 
     # SLICE: Generate slices through the volume at desired depth levels
     slice_vtk = None
@@ -218,23 +289,27 @@ for data_fid in data_fids:
             ClearSelection()
         else:
             slice_vtk = slice_plane(vtk, slice_)
-            tag = f"{slice_}km"
+            tag = f"z_{slice_}km"
 
             # Determine colorbar bounds of slice to rescale colorbar
             vmin, vmax = slice_vtk.PointData.GetArray(0).GetRange(0)
 
+        # Make sure that global bounds are set if required
+        if preset["constant_bounds"]:
+            vmin, vmax = vol_vmin, vol_vmax
+    
         # Some fields should be centered on 0 despite the actual data bounds
         if preset["center_cmap"]:
             vabsmax = max(abs(vmin), abs(vmax))
             vmin, vmax = -1 * vabsmax, vabsmax
+
         # If desired, round the colobar bounds to some base value
         if preset["round_base"]:
             vmin = myround(vmin, preset["round_base"])
             vmax = myround(vmax, preset["round_base"])
-
+       
+        # Apply depth specific values
         vsLUT.RescaleTransferFunction(vmin, vmax)
-
-        # Annotate the depth level
         text.Text = tag
 
         # Ensure that the colorbar shows for each new slice
@@ -251,4 +326,9 @@ for data_fid in data_fids:
             Hide(slice_vtk, renderView)
         else:
             Hide(vtk, renderView)
+
+    # CLEAR: ResetSession() wasnt getting rid of the text so delete manually
+    Delete(text)
+    del text
+    ResetSession()
 
