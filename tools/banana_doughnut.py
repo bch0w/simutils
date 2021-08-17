3bbd8cb538c8c13d2f93c803105f8fe3f2e85fce
bch0w <bryant.h.c@gmail.com>
2021-08-17 22:18:12 +0000

    modified some meshing parameters for Yoshis extended north island mesh

--- tools/banana_doughnut.py
+++ tools/banana_doughnut.py
@@ -5,6 +5,7 @@ kernels using SPECFEM's adjoint simulation
 
 Calculate half duration using a scaling for moment magnitude
 """
+import os
 import sys
 from glob import glob
 import numpy as np
@@ -53,6 +54,34 @@ def plot(tr, fp, window):
     plt.ylabel("amplitude")
     plt.savefig(f"{tr.get_id()}_phase.png", figsize=(20,8), dpi=200)
     plt.close()
+
+
+def manipulate_specfem_rundir():
+    """
+    Place the *.adj files into the SEM directory of the SPECFEM workdir and edit
+    the STATIONS_ADJOINT file in the DATA directory. 
+    """
+    cwd = os.getcwd()
+    assert("OUTPUT_FILES" in cwd), "you are in the wrong directory"
+
+    print("symlinking .adj files into SEM dir")
+    adj_files = [os.path.abspath(_) for _ in glob("*.adj")]
+    os.chdir("../SEM")
+    for src in adj_files:
+        dst = os.path.join(os.getcwd(), os.path.basename(src))
+        os.remove(dst)
+        os.symlink(src, dst)
+   
+    os.chdir("../DATA") 
+    # Assuming that the last symlinked file contains the station name
+    station = os.path.basename(src).split(".")[1]
+    print(f"writing {station} into DATA/STATIONS_ADJOINT")
+    lines = open("STATIONS", "r").readlines()
+    for line in lines:
+        if station in line:
+            with open("STATIONS_ADJOINT", "w") as f:
+                f.write(line)
+            break
     
 
 
@@ -61,13 +90,21 @@ if __name__ == "__main__":
     # DEFINE PARAMETERS HERE
     tmin = 6
     tmax = 30
-    windows = {"Z": [210, 215],}
+    windows = {"Z": [180, 205],}
     half_duration = .7
+    fid = "NZ.RTZ.BX?.semd"
     # =====================================
 
-    if len(sys.argv) < 1:
+    if fid:
+        fids = glob(fid)
+    elif len(sys.argv) >= 1:
+        fids = sys.argv[1:]
+    else:
         sys.exit("Seismogram file id required as argument")
-    fids = sys.argv[1:]
+
+    print("removing existing .adj files")     
+    for fid in glob("*.adj"):
+        os.remove(fid)
 
     for fid in fids:
         # Define naming schema to save the output files
@@ -92,4 +129,6 @@ if __name__ == "__main__":
                                        reverse=True, save=fid_out)
         if not zeros:
             plot(tr, fp, window) 
+
+    manipulate_specfem_rundir()
         
