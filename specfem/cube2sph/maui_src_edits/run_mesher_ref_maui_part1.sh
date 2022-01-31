7c59066916973fe27bdb550502e7cd49929dfdae
bch0w <bryant.h.c@gmail.com>
2022-01-31 20:26:44 +0000

    updates on cube2sph source code edits

--- specfem/cube2sph/maui_src_edits/run_mesher_ref_maui_part1.sh
+++ specfem/cube2sph/maui_src_edits/run_mesher_ref_maui_part1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
-#SBATCH --job-name=xcube2sph 
+#SBATCH --job-name=xcube2sph_p1
 #SBATCH --nodes=1 
-#SBATCH --ntasks=40 
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --clusters=maui
 #SBATCH --account=gns03247
@@ -15,6 +15,9 @@ module switch PrgEnv-cray/6.0.5 PrgEnv-gnu
 module load cray-hdf5/1.10.2.0  
 module load cray-netcdf/4.6.1.3
 
+# This script has been split into two parts because Python can't be run from the
+# main compute cluster and so has to be run on its own from an ancillary node
+# and then the workflow continued
 #################################
 echo "running example: `date`"
 current_dir=`pwd`
@@ -24,7 +27,7 @@ echo
 echo "   setting up example..."
 echo
 ## MODIFY the specfem_dir
-specfem_dir="/scale_wlg_nobackup/filesets/nobackup/gns03247/bchow/tomo/cube2sph/specfem3d_workdir"
+specfem_dir="/scale_wlg_persistent/filesets/project/gns03247/bchow/specfem/cube2sph_specfem3d_b7ed7a33"
 ################################
 cube2sph_dir="cube2sph_utils"
 # cleans output files
@@ -78,30 +81,3 @@ cp DATA/Par_file OUTPUT_FILES/
 
 # prepare for convert HEX8 mesh to HEX27 mesh
 cp MESH-default/* ${cube2sph_dir}/temp
-cd ${cube2sph_dir}
-python3 hex8tohex27.py
-#bash change_name.sh
-for file in temp/*_27; do
-  name=`echo $file|awk -Ftemp/ '{print $2}' |awk -F_27 '{print $1}'` 
-  cp temp/${name}_27 ${current_dir}/MESH-default/${name}
-done
-cd ${current_dir}
-
-# get the number of processors, ignoring comments in the Par_file
-NPROC=`grep ^NPROC DATA/Par_file | grep -v -E '^[[:space:]]*#' | cut -d = -f 2`
-
-BASEMPIDIR=`grep ^LOCAL_PATH DATA/Par_file | cut -d = -f 2 `
-mkdir -p $BASEMPIDIR
-
-# decomposes mesh using the pre-saved mesh files in MESH-default
-echo
-echo "  decomposing mesh..."
-echo
-./bin/xdecompose_mesh $NPROC ./MESH-default $BASEMPIDIR
-# checks exit code
-if [[ $? -ne 0 ]]; then exit 1; fi
-sbatch slurm_cube2sph_mesher.sh
-echo "done"
-echo `date`
-
-
