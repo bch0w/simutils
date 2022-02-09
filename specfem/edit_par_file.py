373d0943a5b1ebb61245e0bb371e555b0ff69c62
Bryant Chow <bryant.h.c@gmail.com>
2022-02-09 11:12:13 -0900

    adding nalaska topography new specfem3d par file editor because im lazy

---
+++ specfem/edit_par_file.py
@@ -0,0 +1,50 @@
+"""
+Tired of having to open the SPECFEM3D Par_file every time you want to edit 
+a silly parameter? This Python script is designed to allow a User top quickly
+access, assess, and edit a parameter in the SPECFEM3D Par_file
+"""
+import sys
+
+
+# Determine if were accessing or editing
+try:
+    name = sys.argv[1]
+except IndexError:
+    sys.exit(f"\n\tfirst argument must be the parameter name "
+             f"(case insensitive)\n")
+try:
+    newval = sys.argv[2]
+except IndexError:
+    newval = None
+
+fid = "./Par_file"
+
+with open(fid, "r") as f:
+    lines = f.readlines()
+
+for i, line in enumerate(lines[:]):
+    # Skip comments and newlines
+    if not line.strip() or line[0] == "#":
+        continue
+    try:
+        key, val, *_ = [_.strip() for _ in line.strip().split("=")]
+        # If comments after the line, remove
+        if "#" in val:
+            val = val.split("#")[0].strip()
+    except ValueError:
+        print(f"\n\tUnexpected line: {line}\t")
+        continue
+    if name.upper() == key:
+        # If accessing, print and move on
+        if newval is None:
+            sys.exit(f"\n\t{line}")
+        else:
+            lines[i] = line.replace(val, newval)
+            print(f"\n\t{key}: {val} -> {newval}\n")
+            break
+else:
+    sys.exit(f"\n\tparameter '{name}' not found\n")
+
+with open(fid, "w") as f:
+    f.writelines(lines)
+
