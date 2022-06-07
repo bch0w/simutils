2c583d78d24cadcdf2abbac840bbf803ffdfc0cd
Bryant Chow <bryant.h.c@gmail.com>
2022-06-07 13:15:28 -0800

    new specfem3d configuration and xcombine_vol_data_vtk scripts running on Chinook

---
+++ cluster/configs/chinook/configure_specfem3d_globe_ifort.sh
@@ -0,0 +1,9 @@
+# From GEOTOOLS/cluster_util/startup/README_specfem
+# Should be run from inside a Specfem repository
+module purge
+module load slurm
+module load compiler/ifort/2018.5.274-GCC-5.4.0-2.26  
+module load openmpi/intel/2.1
+
+./configure FC=ifort CC=icc MPIFC=mpif90 --enable-vectorization
+
