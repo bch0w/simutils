17638bd4165783b851d70c2946987917f03bdce9
bch0w <bryant.h.c@gmail.com>
2023-12-03 15:45:21 -0900

    working on a specfem3d_globe script for plotting movies, now with maps with cartopy

--- cluster/runscripts/linux/run_xcombine_vol_data_vtk.sh
+++ cluster/runscripts/linux/run_xcombine_vol_data_vtk.sh
@@ -8,7 +8,7 @@
 SLICE_LIST="all"
 FILENAME=$1  # or filename, use $1 to use command line argumnent
 INPUT_TOPO_DIR="DATABASES_MPI/"
-INPUT_FILE_DIR="SUM/"
+INPUT_FILE_DIR="DATABASES_MPI/"
 OUTPUT_DIR="OUTPUT_FILES/"
 RES=0  # 0 for low-res, 1 for hi-res (hi-res is LARGE)
 REGION=1
