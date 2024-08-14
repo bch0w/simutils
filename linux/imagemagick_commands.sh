7de47d577f111fa34bbcdd268e21db4d49a84148
bch0w <bryant.h.c@gmail.com>
2024-08-14 12:20:51 -0800

    newstuff

--- linux/imagemagick_commands.sh
+++ linux/imagemagick_commands.sh
@@ -1,3 +1,6 @@
 # Uses Imagemagcik to reduce file size on gifs, fuzz blends similar colors 
 # together. Can also be used on images I guess
 mogrify -layers 'optimize' -fuzz 5% ${1}
+
+# Convert pngs to gif
+magick -loop 0 *.png output.gif
---
+++ specfem/specfem++/plot_kernels.py
@@ -0,0 +1,8 @@
+"""
+Plot 2D kernels out of SPECFEM++
+"""
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
