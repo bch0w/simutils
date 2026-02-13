557a76bc4129b7002b2a6f7b900ea2c122dfa02f
bch0w <bryant.h.c@gmail.com>
2026-02-12 17:53:13 -0900

    backing up model building script and config

--- meshing/specfem_tomo_helper/make_fwea23_model_w_stoch_perts.sh
+++ meshing/specfem_tomo_helper/make_fwea23_model_w_stoch_perts.sh
@@ -1,3 +1,4 @@
+#1/bin/bash -e
 # Used to make perturbed FWEA23 model for Simblast work
 # 2/10/26
 # 1) Update CONFIGS/* for the shallow (perturbed) model and rest of mesh
@@ -10,17 +11,21 @@
 # 6) Manually upload to SPECFEM DATA/ directory
 # 7) Modify SPECFEM Par_file to match `NPROC`
 
-mamba activate specfem_tomo_helper
-
 # Set up file system
 rm -rf EXPORT
 mkdir -p EXPORT/tomo_files
+mkdir -p EXPORT/tomo_files_unperturbed
 mkdir -p EXPORT/meshfem3D_files
+rm -rf TMP/*
 cp -r CONFIGS EXPORT
 
 # Make tomography and mesh files
 tomo-helper -c CONFIGS/whole_model_config_fwea23.yaml 
 tomo-helper -c CONFIGS/shallow_only_config_fwea23.yaml
+
+# Set up unperturbed model for ref. seismograms
+cp -r TMP/WHOLE/tomo_files/tomography_model.xyz EXPORT/tomo_files_unperturbed/tomography_model_2.xyz
+cp -r TMP/SHALLOW/tomo_files/tomography_model.xyz EXPORT/tomo_files_unperturbed/tomography_model_1.xyz
 
 # Apply stochastic perturbations
 python /Users/chow/Repos/simutils/meshing/stochperts/apply_stochpert_xyz.py \
