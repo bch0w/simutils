507029f8c081de6f4600f752ae40eb886a35ccb4
bch0w <bryant.h.c@gmail.com>
2026-04-14 15:11:02 -0800

    compleixfiying specfem run tool

--- cluster/runscripts/shadowfax/specfem3d/run_all_specfem.sh
+++ cluster/runscripts/shadowfax/specfem3d/run_all_specfem.sh
@@ -6,28 +6,57 @@
 #SBATCH --time=02:30:00
 #SBATCH --output=LOGS/simblast_%j.out
 
-# Get the number of processors from the Par_file, ignore comments
+# Establish some run parameters
+# Expand CMTSOLUTION symlink which will be tagged with the event name
+TAG=readlink -f DATA/CMTSOLUTION | grep -oP '[^/]*$' | grep -oP '(?<=_)[^_]*$'
+NSTEP=`grep ^NSTEP DATA/Par_file | grep -v -E '^[[:space:]]*#' | cut -d = -f 2`
 NPROC=${SLURM_NTASKS}
-DATABASES=OUTPUT_FILES/DATABASES_MPI
-mkdir -p ${DATABASES}
+OUTPUT_FILES="OUTPUT_FILES"
+DATABASES="${OUTPUT_FILES}/DATABASES_MPI"
+RESULTS="RESULTS/${TAG}/"
 
-echo "${NPROC} TASKS"
+mkdir -p ${DATABASES}
+mkdir -p ${RESULTS}
+
+# Copy over some data
+cp DATA/CMTSOLUTION ${RESULTS}
+cp DATA/Par_file ${RESULTS}
+
+echo "RUNNING ${TAG} WITH ${NPROC} TASKS"
 echo "starting at: `date`"
 
 # Run xmeshfem3D if it hasn't been run yet
 if [ ! -f "${DATABASES}/proc000000_Database" ]; then
 	echo "======== Running xmeshfem3D ========"
 	time mpiexec -n ${NPROC} ./bin/xmeshfem3D
+	cp ${OUTPUT_FILES}/output_meshfem3D.txt ${RESULTS}
 fi
 
 # Run xgenenerate_databases if it hasn't been run yet
 if [ ! -f "${DATABASES}/proc000000_vs.bin" ]; then
 	echo "======== Running xgenerate_databases ========"	
 	time mpiexec -n ${NPROC} ./bin/xgenerate_databases
+	cp ${OUTPUT_FILES}/output_generate_databases.txt ${RESULTS}
 fi
 
-# Always run the solver
+# Run the solver if no waveform files exist
 echo "======== Running xspecfem3D ========"
-time mpiexec -n ${NPROC} ./bin/xspecfem3D
+if ! compgen -G "${OUTPUT_FILES}/*.semv" > /dev/null; then
+	time mpiexec -n ${NPROC} ./bin/xspecfem3D
+	cp ${OUTPUT_FILES}/output_solver.txt ${RESULTS}
+	echo "finished at: `date`"
+fi
 
-echo "finished at: `date`"
+# Make ParaView movie files if needed
+echo "======== Running xcreate_move_shakemap_AVS_DX_GMT ========"
+if ! compgen -G "${OUTPUT_FILES}/moviedata*" > /dev/null; then
+	./bin/xcreate_movie_shakemap_AVS_DX_GMT << EOF
+	2
+	1
+	${NSTEP}
+	2
+	4
+	EOF
+
+
+
--- cluster/runscripts/shadowfax/specfem3d/xcreate_movie_shakemap_AVS_DX_GMT.sh
+++ cluster/runscripts/shadowfax/specfem3d/xcreate_movie_shakemap_AVS_DX_GMT.sh
@@ -1,13 +1,22 @@
-#!/bin/sh                                                                        
-                                                                                 
-# PARAMETERS
-# 5 = create files in VTU format with individual files
-# 1 = first time step
-# -1 = last time step
-# 1 = Z  (1=Z, 2=N, 3=E)
-echo "xcreate_movie_shakemap_AVS_DX_GMT"
-echo
-echo "`date`"
-./bin/xcreate_movie_shakemap_AVS_DX_GMT  <<< $'5\n1\n-1\n1'
-echo
-echo "finished at: `date`"
+#!/bin/bash
+# Line 1: Velocity will be shown in movie
+# 	1 = create files in OpenDX format
+# 	2 = create files in AVS UCD format
+# 	3 = create files in GMT xyz Ascii long/lat/Uz format
+# 	    any other value = exit
+# Line 2: Enter first time step of movie
+# Line 3: Enter last time step of movie
+# Line 4:  1= norm of velocity  2=velocity x-comp 3=velocity y-comp 4=velocity z-comp
+# Line 5: 
+#	1 = define file names using frame number
+# 	2 = define file names using time step number
+
+NSTEP=`grep ^NSTEP DATA/Par_file | grep -v -E '^[[:space:]]*#' | cut -d = -f 2`
+
+mpiexec -n 16 ./bin/xcreate_movie_shakemap_AVS_DX_GMT << EOF
+2
+1
+$NSTEP
+2
+4
+EOF
